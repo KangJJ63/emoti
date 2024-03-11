f5943d562da327bc752f869090ed4bb9fd080480
JyunDev <83933219+Jyundev@users.noreply.github.com>
2024-03-11 15:16:32 +0900

    initial

---
+++ src/main/java/com/mini/emoti/EmotiApplication.java
@@ -0,0 +1,13 @@
+package com.mini.emoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotiApplication.class, args);
+	}
+
+}
